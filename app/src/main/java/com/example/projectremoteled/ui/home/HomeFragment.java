0792e0c6fff38b07272b9085a70a37c661369b70
dmz182s <dmz1822s@gmail.com>
2022-07-06 00:57:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectremoteled/ui/home/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.example.projectremoteled.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.projectremoteled.databinding.FragmentHistoryBinding;
+import com.example.projectremoteled.databinding.FragmentHomeBinding;
+import com.example.projectremoteled.ui.history.HistoryViewModel;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.switchButton;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
